3f927ab649d0f6b3e29622ca3c22ba6dbf1e970e
Eric Caleb <calebme@mail.com>
2016-12-25 08:41:53 +0000

    Modified string manipulation library

--- stringlib.c
+++ stringlib.c
@@ -4,5 +4,16 @@
 #include <string.h>
 
 int main(void) {
-  
+    char s1[20];
+    char s2[20];
+
+    strcpy(s1, "Christmas");
+    strcpy(s2, "Santa Claus");
+
+    int len1 = strlen(s1);
+    printf("Length of string: %d\n",len1);
+    int len2 = strlen(s2);
+    printf("Length of string: %d\n",len2);
+
+    printf("Return value: %d\n", strcmp(s1, s2));
 }
