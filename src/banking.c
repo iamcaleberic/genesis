98b67917525fc30fb55e7358e502d1e057fe41fd
Eric Caleb <calebme@mail.com>
2016-12-27 12:04:54 +0000

    Added external library & main efunction

---
+++ src/banking.c
@@ -0,0 +1,21 @@
+#include "banking.h"
+
+int main(){
+  initalizeAccount();
+  getBalance();
+
+  askCustomer();
+  updateAccount(amount);
+  getBalance();
+
+  askCustomer();
+  updateAccount(amount);
+  addGift(15.0);
+  getBalance();
+
+  askCustomer();
+  updateAccount(amount);
+  addGift(2.0);
+  getBalance();
+  thankYou();
+}
