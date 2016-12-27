49ebbd7e9310360087c5cd24cd9631218cb8495c
Eric Caleb <calebme@mail.com>
2016-12-27 09:30:57 +0000

    Added function prototypes & global vars

---
+++ src/functional.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void assessSpaceshipCapacity(void);
+void estimateWaitingVisitors(void);
+void processData(void);
+void generateReport(void);
+
+int spaceship1, spaceship2, spaceship3;
+int visitors1, visitors2, visitors3;
+int totalAvailableSeats, totalWaitingVisitors;
+
+int main(void){
+
+}
