a169900630b400cc452cc650b5fcc09ef4e54e2b
Eric Caleb <calebme@mail.com>
2016-12-29 14:10:47 +0000

    Added while | for loops examples

---
+++ src/loops.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+void forLoop(void);
+void whileLoop(int n);
+
+#define TRUE  1
+#define FALSE 0
+
+int main(void){
+  forLoop();
+  whileLoop(1);
+};
+
+void forLoop(void){
+  for (int i = 0; i < 10; i+=2) {
+    printf("%d\n",i );
+  };
+}
+
+void whileLoop(int n){
+  int flag = FALSE;
+  while (!flag) {
+    printf("%d\n",(n) );
+    n++;
+    if (n >= 10)
+      flag = TRUE;
+  }
+}
