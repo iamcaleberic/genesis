657e9e43cbeea4d137f37501e4cfe822639d7987
Eric Caleb <calebme@mail.com>
2016-12-31 07:18:08 +0000

    Added human struct

---
+++ src/struct.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_NAME  30
+#define MAX_COLOR 15
+
+struct human {
+  char firstName[MAX_NAME];
+  char lastName[MAX_NAME];
+  char eyeColor[MAX_COLOR];
+  float height;
+  int age;
+
+};
+
+void printStructure(struct human individual);
+
+int main(void){
+
+
+}
