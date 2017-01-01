806f8f59130a028595c4a01408dcfe8e804711f6
Eric Caleb <calebme@mail.com>
2017-01-01 12:18:35 +0000

    Added main to run seqfiles functions

--- src/seqfiles.c
+++ src/seqfiles.c
@@ -1,36 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void readEntireFile(void);
-void readUntilMarker(char marker);
+#include "seqfunctions.h"
 
 int main(void){
   readEntireFile();
   printf("---------\n" );
   readUntilMarker('p');
+  writeToFile();
 
-}
-
-void readEntireFile(){
-  int ch;
-  FILE *fp; // pointer to file
-  fp = fopen("/home/caleb/Documents/genesis/data/me", "r");
-  ch = getc(fp);
-  while (ch != EOF){
-    putchar(ch);
-    ch = getc(fp);
-  }
-  fclose(fp); // close files
-}
-
-void readUntilMarker(char marker){
-  int ch;
-  FILE *fp;
-  fp = fopen("/home/caleb/Documents/genesis/data/me", "r");
-  ch = getc(fp);
-  while(ch != marker){
-    putchar(ch);
-    ch = getc(fp);
-  }
-  fclose(fp);
 }
