4f8ffab788b6c35a3a5a701b2f03ce875f671102
Eric Caleb <calebme@mail.com>
2017-01-01 14:33:32 +0000

    Refactored hello

--- src/hello.c
+++ src/hello.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main(void){
-	puts("!!!Hello World");
+	puts("!!!Hello Math");
+	int number;
+	int square;
 
-	return EXIT_SUCCESS;
+	square = sqrt(4);
+
+	printf("Square root of your number is: %d\n",square );
+
+	// return EXIT_SUCCESS;
 }
