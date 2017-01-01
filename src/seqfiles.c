ff58866aa640577466ba88837539685203351dc7
Eric Caleb <calebme@mail.com>
2017-01-01 12:10:22 +0000

    Added readuntil marker function

--- src/seqfiles.c
+++ src/seqfiles.c
@@ -2,17 +2,33 @@
 #include <stdlib.h>
 
 void readEntireFile(void);
+void readUntilMarker(char marker);
 
 int main(void){
   readEntireFile();
+  printf("---------\n" );
+  readUntilMarker('p');
+
 }
 
 void readEntireFile(){
   int ch;
-  FILE *fp;
+  FILE *fp; // pointer to file
   fp = fopen("/home/caleb/Documents/genesis/data/me", "r");
   ch = getc(fp);
   while (ch != EOF){
+    putchar(ch);
+    ch = getc(fp);
+  }
+  fclose(fp); // close files
+}
+
+void readUntilMarker(char marker){
+  int ch;
+  FILE *fp;
+  fp = fopen("/home/caleb/Documents/genesis/data/me", "r");
+  ch = getc(fp);
+  while(ch != marker){
     putchar(ch);
     ch = getc(fp);
   }
